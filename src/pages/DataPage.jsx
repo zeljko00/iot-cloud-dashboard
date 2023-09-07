a6056de0d317ebd0454a57119f8c501e12ae3b08
zeljko00 <tripic.zeljko@gmail.com>
2023-09-07 23:13:41 +0200

    live data bug fixed

--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, PureComponent } from "react";
+import React, { useEffect, useState, PureComponent, useRef } from "react";
 import ScaleIcon from "@mui/icons-material/Scale";
 import LocalGasStationIcon from "@mui/icons-material/LocalGasStation";
 import Button from "@mui/material/Button";
@@ -35,40 +35,42 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 const colors = ["#ffc658", "#82ca9d"];
 
 export const DataPage = () => {
-
   const [tempData, setTempData] = useState(null);
   const [loadData, setLoadData] = useState(null);
   const [fuelData, setFuelData] = useState(null);
-  let overallData = {}
+  const tempDataRef = useRef([]);
+  const loadDataRef = useRef([]);
+  const fuelDataRef = useRef([]);
+  const [stats, setStats] = useState(null);
   const [snackbar, setSnackbar] = useState(false);
-  const [errorMsg, setErrorMsg] = useState("")
+  const [errorMsg, setErrorMsg] = useState("");
 
-  let lastTempTime = ""
-  let lastFuelTime = ""
-  let lastLoadTime = ""
+  const lastTempTime = useRef("");
+  const lastFuelTime = useRef("");
+  const lastLoadTime = useRef("");
 
   const navigate = useNavigate();
 
   const changeTempData = (data) => {
-    if (data.temperatureData.length > 0) {
-      const tempDataObj = handleTempData(data)
+    if (data.length > 0) {
+      const tempDataObj = handleTempData(data);
       setTempData(tempDataObj);
     }
-  }
+  };
   const changeLoadData = (data) => {
-    if (data.loadData.length > 0) {
-      const loadDataObj = handleLoadData(data)
+    if (data.length > 0) {
+      const loadDataObj = handleLoadData(data);
       setLoadData(loadDataObj);
     }
-  }
+  };
   const changeFuelData = (data) => {
-    if (data.fuelData.length > 0) {
-      const fuelDataObj = handleFuelData(data)
+    if (data.length > 0) {
+      const fuelDataObj = handleFuelData(data);
       setFuelData(fuelDataObj);
     }
-  }
+  };
   const handleTempData = (data) => {
-    const graphData = data.temperatureData.sort((a, b) => {
+    const graphData = data.sort((a, b) => {
       return a.time.localeCompare(b.time);
     });
     const currentTemp = graphData[graphData.length - 1].value;
@@ -86,19 +88,6 @@ export const DataPage = () => {
     })[0].time;
     const maxPerc = maxTemp / avgTemp;
     const currentPerc = currentTemp / avgTemp;
-    let collectedData = 1; let usedData = 1; let requests = 1; let reductionPerc = 1
-    if (data.deviceStats.length > 0) {
-      collectedData = data.deviceStats.reduce((init, b) => {
-        return init + b.tempDataBytes;
-      }, 0);
-      usedData = data.deviceStats.reduce((init, b) => {
-        return init + b.tempDataBytesForwarded;
-      }, 0);
-      requests = data.deviceStats.reduce((init, b) => {
-        return init + b.tempDataRequests;
-      }, 0);
-      reductionPerc = usedData / collectedData;
-    }
     const tempDataObj = {
       graphData,
       currentTemp,
@@ -107,15 +96,11 @@ export const DataPage = () => {
       maxTemp,
       maxTime,
       maxPerc,
-      collectedData,
-      usedData,
-      reductionPerc,
-      requests,
     };
     return tempDataObj;
-  }
+  };
   const handleLoadData = (data) => {
-    const graphData = data.loadData.sort((a, b) => {
+    const graphData = data.sort((a, b) => {
       return a.time.localeCompare(b.time);
     });
     const currentLoad = graphData[graphData.length - 1].value;
@@ -128,19 +113,6 @@ export const DataPage = () => {
     }, 0);
     const sumPerc = sumLoad / avgLoad;
     const currentPerc = currentLoad / avgLoad;
-    let collectedData = 1; let usedData = 1; let requests = 1; let reductionPerc = 1;
-    if (data.deviceStats.length > 0) {
-      collectedData = data.deviceStats.reduce((init, b) => {
-        return init + b.loadDataBytes;
-      }, 0);
-      usedData = data.deviceStats.reduce((init, b) => {
-        return init + b.loadDataBytesForwarded;
-      }, 0);
-      requests = data.deviceStats.reduce((init, b) => {
-        return init + b.loadDataRequests;
-      }, 0);
-      reductionPerc = usedData / collectedData;
-    }
     const loadDataObj = {
       graphData,
       currentLoad,
@@ -148,16 +120,12 @@ export const DataPage = () => {
       avgLoad,
       sumLoad,
       sumPerc,
-      collectedData,
-      usedData,
-      reductionPerc,
-      requests,
     };
     return loadDataObj;
-  }
+  };
 
   const handleFuelData = (data) => {
-    const graphData = data.fuelData.sort((a, b) => {
+    const graphData = data.sort((a, b) => {
       return a.time.localeCompare(b.time);
     });
     const lastCriticalFuel = graphData[graphData.length - 1].value;
@@ -173,35 +141,23 @@ export const DataPage = () => {
     let empty = 0;
     if (minFuel === 0) empty = mins.length;
     const minTime = mins[mins.length - 1].time;
-    let collectedData = 1; let usedData = 1; let requests = 1; let reductionPerc = 1;
-    if (data.deviceStats.length > 0) {
-      collectedData = data.deviceStats.reduce((init, b) => {
-        return init + b.fuelDataBytes;
-      }, 0);
-      usedData = data.deviceStats.reduce((init, b) => {
-        return init + b.fuelDataBytesForwarded;
-      }, 0);
-      requests = data.deviceStats.reduce((init, b) => {
-        return init + b.fuelDataRequests;
-      }, 0);
-      reductionPerc = usedData / collectedData;
-    }
     const fuelDataObj = {
       graphData,
       lastCriticalFuel,
       lastCriticalTime,
       minFuel,
       minTime,
-      collectedData,
-      usedData,
-      reductionPerc,
-      requests,
       empty,
     };
     return fuelDataObj;
-  }
+  };
   useEffect(() => {
-    if (!sessionStorage.getItem("jwt") || sessionStorage.getItem("jwt") === "" || !sessionStorage.getItem("device") || sessionStorage.getItem("device") === "")
+    if (
+      !sessionStorage.getItem("jwt") ||
+      sessionStorage.getItem("jwt") === "" ||
+      !sessionStorage.getItem("device") ||
+      sessionStorage.getItem("device") === ""
+    )
       navigate("/iot-platform/login");
     axios
       .get(conf.server_url + `/data`, {
@@ -210,19 +166,57 @@ export const DataPage = () => {
         },
       })
       .then((res) => {
-        // console.log(res);
-        overallData = res.data;
-        console.log(res.data)
-        changeFuelData({fuelData: res.data.fuelData, deviceStats: res.data.deviceStats})
-        changeTempData({temperatureData: res.data.temperatureData, deviceStats: res.data.deviceStats})
-        changeLoadData({loadData: res.data.loadData, deviceStats: res.data.deviceStats})
+        // console.log(res.data);
+        if (res.data.deviceStats.length > 0) {
+          const collectedTempData = res.data.deviceStats.reduce((init, b) => {
+            return init + b.tempDataBytes;
+          }, 0);
+          const usedTempData = res.data.deviceStats.reduce((init, b) => {
+            return init + b.tempDataBytesForwarded;
+          }, 0);
+          const reductionTempPerc =
+            collectedTempData !== 0 ? usedTempData / collectedTempData : 0;
+          const collectedLoadData = res.data.deviceStats.reduce((init, b) => {
+            return init + b.loadDataBytes;
+          }, 0);
+          const usedLoadData = res.data.deviceStats.reduce((init, b) => {
+            return init + b.loadDataBytesForwarded;
+          }, 0);
+          const reductionLoadPerc =
+            collectedLoadData !== 0 ? usedLoadData / collectedLoadData : 0;
+
+          const collectedFuelData = res.data.deviceStats.reduce((init, b) => {
+            return init + b.fuelDataBytes;
+          }, 0);
+          const usedFuelData = res.data.deviceStats.reduce((init, b) => {
+            return init + b.fuelDataBytesForwarded;
+          }, 0);
+          const reductionFuelPerc = usedFuelData / collectedFuelData;
+          setStats({
+            collectedTempData: collectedTempData - usedTempData,
+            usedTempData,
+            reductionTempPerc,
+            collectedLoadData: collectedLoadData - usedLoadData,
+            usedLoadData,
+            reductionLoadPerc,
+            collectedFuelData: collectedFuelData - usedFuelData,
+            usedFuelData,
+            reductionFuelPerc,
+          });
+        }
+        tempDataRef.current = res.data.temperatureData;
+        loadDataRef.current = res.data.loadData;
+        fuelDataRef.current = res.data.fuelData;
+        changeFuelData(res.data.fuelData);
+        changeTempData(res.data.temperatureData);
+        changeLoadData(res.data.loadData);
         connect();
       })
       .catch((exc) => {
-        let msg = ""
+        let msg = "";
         if (exc.response.status === 401) msg = "Unauthorized access!";
-        else msg = "Server unreachable!"
-        setErrorMsg(msg)
+        else msg = "Server unreachable!";
+        setErrorMsg(msg);
         setSnackbar(true);
       });
   }, []);
@@ -240,70 +234,53 @@ export const DataPage = () => {
     //timeout after establishing connection required from unknown reasons
     setTimeout(() => {
       // subscribing user to topic that stores active users
-      stompClient.subscribe("/devices/" + sessionStorage.getItem("device") + "/load", (payload) => {
-        const body = JSON.parse(payload.body)
-        if (body.time !== lastLoadTime) {
-          
-          try {
-            if (overallData.loadData !== null)
-              overallData.loadData.push(body)
-            else
-              overallData.loadData = [body]
-          } catch (e) {
-            overallData.loadData = [body]
+      stompClient.subscribe(
+        "/devices/" + sessionStorage.getItem("device") + "/load",
+        (payload) => {
+          const body = JSON.parse(payload.body);
+          if (body.time !== lastLoadTime.current) {
+            loadDataRef.current.push(body);
+            console.log("-----------------LOAD---------------------");
+            console.log(loadDataRef.current);
+            console.log("------------------------------------------");
+            changeLoadData(loadDataRef.current);
           }
-          console.log("-----------------LOAD---------------------")
-          console.log({loadData: overallData.loadData, deviceStats: overallData.deviceStats})
-          console.log("------------------------------------------")
-          changeLoadData({loadData: overallData.loadData, deviceStats: overallData.deviceStats})
+          lastLoadTime.current = body.time;
         }
-        lastLoadTime = body.time
-      }
       );
-      stompClient.subscribe("/devices/" + sessionStorage.getItem("device") + "/fuel_level", (payload) => {// console.log("Fuel:" + payload)
-        const body = JSON.parse(payload.body);
-        if (body.time !== lastFuelTime) {
-          try {
-            if (overallData.fuelData != null)
-              overallData.fuelData.push(body)
-            else
-              overallData.fuelData = [body]
-          } catch (e) {
-            overallData.fuelData = [body]
+      stompClient.subscribe(
+        "/devices/" + sessionStorage.getItem("device") + "/fuel_level",
+        (payload) => {
+          const body = JSON.parse(payload.body);
+          if (body.time !== lastFuelTime.current) {
+            fuelDataRef.current.push(body);
+            console.log("-----------------FUEL---------------------");
+            console.log(fuelDataRef.current);
+            console.log("------------------------------------------");
+            changeFuelData(fuelDataRef.current);
           }
-          console.log("-------------------FUEL-------------------")
-          console.log({fuelData: overallData.fuelData, deviceStats: overallData.deviceStats})
-          console.log("------------------------------------------")
-          changeFuelData({fuelData: overallData.fuelData, deviceStats: overallData.deviceStats})
+          lastFuelTime.current = body.time;
         }
-        lastFuelTime = body.time
-      }
       );
-      stompClient.subscribe("/devices/" + sessionStorage.getItem("device") + "/temperature", (payload) => {// console.log("Temp:" + payload)
-        const body = JSON.parse(payload.body);
-        if (body.time !== lastTempTime) {
-          
-          try {
-            if (overallData.temperatureData != null)
-              overallData.temperatureData.push(body)
-            else
-              overallData.temperatureData = [body]
-          } catch (e) {
-            overallData.temperatureData = [body]
+      stompClient.subscribe(
+        "/devices/" + sessionStorage.getItem("device") + "/temperature",
+        (payload) => {
+          const body = JSON.parse(payload.body);
+          if (body.time !== lastTempTime.current) {
+            tempDataRef.current.push(body);
+            console.log("-----------------TEMP---------------------");
+            console.log(tempDataRef.current);
+            console.log("------------------------------------------");
+            changeTempData(tempDataRef.current);
           }
-          console.log("------------------TEMP--------------------")
-          console.log({temperatureData: overallData.temperatureData, deviceStats: overallData.deviceStats})
-          console.log("------------------------------------------")
-          changeTempData({temperatureData: overallData.temperatureData, deviceStats: overallData.deviceStats})
+          lastTempTime.current = body.time;
         }
-        lastTempTime = body.time
-      }
       );
     }, 100);
   };
   const onError = () => {
-    setErrorMsg("Live data request failed!")
-    setSnackbar(true)
+    setErrorMsg("Live data request failed!");
+    setSnackbar(true);
   };
   const connect = () => {
     const socket = new SocketJS(conf.server_url + "/ws");
@@ -366,7 +343,7 @@ export const DataPage = () => {
             subtitle="(last hour)"
             valueColor="green-value"
             value={
-              tempData != null ? tempData.avgTemp.toFixed(3) + "°C" : "Unknown"
+              tempData != null ? tempData.avgTemp.toFixed(1) + "°C" : "Unknown"
             }
             second=""
             positive={false}
@@ -388,9 +365,9 @@ export const DataPage = () => {
           <StatsCard
             title="Device temp stats"
             subtitle="Useful data"
-            valueA={tempData != null ? tempData.collectedData : -1}
-            valueB={tempData != null ? tempData.usedData : -1}
-            second={tempData != null ? tempData.reductionPerc : -1}
+            valueA={stats != null ? stats.collectedTempData : -1}
+            valueB={stats != null ? stats.usedTempData : -1}
+            second={stats != null ? stats.reductionTempPerc : -1}
           />
         </div>
         <div className="graph-area">
@@ -461,7 +438,7 @@ export const DataPage = () => {
             subtitle="(last hour)"
             valueColor="red-value"
             value={
-              loadData != null ? loadData.sumLoad.toFixed(1) + "kg" : "Unknown"
+              loadData != null ? loadData.sumLoad.toFixed(0) + "kg" : "Unknown"
             }
             second=""
             positive={true}
@@ -469,9 +446,9 @@ export const DataPage = () => {
           <StatsCard
             title="Device load stats"
             subtitle="Useful data"
-            valueA={loadData != null ? loadData.collectedData : -1}
-            valueB={loadData != null ? loadData.usedData : -1}
-            second={loadData != null ? loadData.reductionPerc : -1}
+            valueA={stats != null ? stats.collectedLoadData : -1}
+            valueB={stats != null ? stats.usedLoadData : -1}
+            second={stats != null ? stats.reductionLoadPerc : -1}
           />
         </div>
         <div className="graph-area">
@@ -550,36 +527,14 @@ export const DataPage = () => {
           <StatsCard
             title="Device fuel stats"
             subtitle="Useful data"
-            valueA={fuelData != null ? fuelData.collectedData : -1}
-            valueB={fuelData != null ? fuelData.usedData : -1}
-            second={fuelData != null ? fuelData.reductionPerc : -1}
+            valueA={stats != null ? stats.collectedFuelData : -1}
+            valueB={stats != null ? stats.usedFuelData : -1}
+            second={stats != null ? stats.reductionFuelPerc : -1}
           />
         </div>
         <div className="graph-area">
           {fuelData && (
             <ResponsiveContainer width="100%" height="100%">
-              {/* <AreaChart
-                width={500}
-                height={400}
-                data={fuelData.graphData}
-                margin={{
-                  top: 10,
-                  right: 30,
-                  left: 0,
-                  bottom: 0,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="time" />
-                <YAxis />
-                <Tooltip />
-                <Area
-                  type="monotone"
-                  dataKey="value"
-                  stroke="#8884d8"
-                  fill="#8884d8"
-                />
-              </AreaChart> */}
               <BarChart
                 width={500}
                 height={300}
